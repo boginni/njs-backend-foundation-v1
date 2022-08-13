190539046079898c7071905d9c74ed8f7951226c
boginni <brunnomarqueszr1@gmail.com>
2022-08-13 19:22:02 -0300

    testing

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-
+fs = require('fs')
 
 exports.printMsg = function() {
     console.log("This is a message from the demo package");
+
+
+    fs.writeFileSync('test.json', JSON.stringify({"test" : true}, null, 2 ))
+
+
 }
+
+
+
+
+
